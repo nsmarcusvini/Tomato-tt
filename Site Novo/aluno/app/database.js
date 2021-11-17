cd2941f236a574e1e302e4d776a116e18ffa479a
chris-lock2 <christian.assis@bandtec.com.br>
2021-11-17 14:41:49 -0300

    api finalizada

---
+++ Site Novo/aluno/app/database.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    port     : '3306',
+    user     : 'root',
+    password : 'bandtec',
+    database : 'tomatopi'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('Conectado com sucesso!')
+});
+
+
+
+module.exports = connection;
